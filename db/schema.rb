0cb68e818ba461e3a9dd0847856901c34bf92317
Anastasiia Makarova <erdbeer@yandex.ru>
2020-06-25 17:53:21 +0100

    Create Model Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_120704) do
+ActiveRecord::Schema.define(version: 2020_06_25_144718) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
